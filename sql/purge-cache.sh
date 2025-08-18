b7d93da90e7ef46c241ad4a5b2b585171ec08905
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-08-18 16:28:28 +0000

    updated purge-cache.sh env path

--- sql/purge-cache.sh
+++ sql/purge-cache.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Load the .env file from the same directory
 set -a
-source "$SCRIPT_DIR/.env"
+source "$SCRIPT_DIR/secret.env"
 set +a
 mysql -u root -p"$DB_PW" "$DB_NAME_LOGGER" < "$SCRIPT_DIR/purge-cache.sql"
